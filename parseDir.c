9a2c6b4a8336bd92a99a5180ebf8d288879c54c4
dddelacruz <dddelacruz7@up.edu.ph>
2024-01-14 02:30:20 +0800

    cleaned printf

--- parseDir.c
+++ parseDir.c
@@ -29,7 +29,7 @@ uint readInt(FILE *fs, uint offset, uint size){
     //little endian
     for(int i = 0; i < size; i++){
         ret |= buffer[i] << (i*8);
-        printf("addr: %u\nbuffer[%d]: %u\nret: %u\n\n",SB_START+offset, i, buffer[i], ret);
+        //printf("addr: %u\nbuffer[%d]: %u\nret: %u\n\n",SB_START+offset, i, buffer[i], ret);
     }
 
     return ret;
--- test.c
+++ test.c
@@ -5,8 +5,8 @@ void test_parseDir(FILE * fs){
     superblock * sb = parseSuperBlock(fs);
     
     printf("block size: %u\n", sb->block_sz);
-    printf("number of blocks: %u\n", sb->block_num);
-    printf("number of inodes: %u\n", sb->inode_num);
+    printf("number of blocks per group: %u\n", sb->block_num);
+    printf("number of inodes per group: %u\n", sb->inode_num);
     printf("BGDT block: %u\n", sb->bgdt_block);
     printf("inode size: %u\n", sb->inode_sz);
     
