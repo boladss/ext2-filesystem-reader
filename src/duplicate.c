9ed32e5a5d8a836a9a26c7b28f4a0f24003d988f
dddelacruz <dddelacruz7@up.edu.ph>
2024-01-20 12:41:41 +0800

    cleaned code and moved c files to own folder

---
+++ src/duplicate.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "parseDir.c"
+
+// copy data block from fs to f
+void copyBlock(int fs, int f, superblock * sb, inode * in, uint block_num, uint * bytes_read){
+    uchar data_buf[sb->block_sz];
+    uchar curr[1];
+
+    getDataBlock(fs, sb, block_num, data_buf);
+    int offset = 0;
+
+    while(offset < sb->block_sz && *bytes_read < in->file_sz){
+        curr[0] = data_buf[offset];
+        write(f, curr, 1);
+        offset++;
+        (*bytes_read)++;
+    }
+}
+
+void copySingleIndBlock(int fs, int f, superblock * sb, inode * in, uint block_num, uint * bytes_read){
+    for(int i = 0; i < sb->block_sz; i+=4){
+        uint curr_offset = (block_num*sb->block_sz)+i; // address of direct pointer
+        uint block_num = readInt(fs, curr_offset, 4); // data block number
+        
+        if(block_num == 0) continue;
+
+        copyBlock(fs, f, sb, in, block_num, bytes_read);
+    }
+}
+
+void duplicateFile(int fs, superblock * sb, inode * in, char * name){
+    // create new file
+    int f = open(name, O_CREAT | O_WRONLY, 0777);
+
+    // write to new file
+    // go through all data blocks of original file
+    //get inode of file
+    // for each data block: write to new file
+
+    uint bytes_read = 0;
+
+    //direct pointers
+    for(int i = 0; i < 12; i++){
+        if(in->direct[i] == 0) continue; //skip empty pointers
+
+        copyBlock(fs, f, sb, in, in->direct[i], &bytes_read);
+    }
+
+    //indirect pointers
+    //single
+    if(in->single_ind){
+        copySingleIndBlock(fs, f, sb, in, in->single_ind, &bytes_read);
+    }
+
+    //double
+    if(in->double_ind){ 
+        // handler for double indirect block
+        // contains pointers to block containing singly indirect blocks
+
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get double block
+
+            uint d_block = (in->double_ind*sb->block_sz)+i; // address of direct pointer
+            uint d_block_num = readInt(fs, d_block, 4); // data block number
+
+           copySingleIndBlock(fs, f, sb, in, d_block_num, &bytes_read);
+        }
+    }
+
+    //triple
+    if(in->triple_ind){
+        // handler for triple indirect block
+        // contains pointers to blocks containing doubly indirect blocks
+
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get triple block
+            uint t_block = (in->triple_ind*sb->block_sz)+i;
+            uint t_block_num = readInt(fs, t_block, 4); // data block number
+
+            for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+                uint d_block = (t_block_num*sb->block_sz)+i; // address of direct pointer
+                uint d_block_num = readInt(fs, d_block, 4); // data block number
+
+                copySingleIndBlock(fs, f, sb, in, d_block_num, &bytes_read);
+            }
+        }
+    }
+
+    close(f);
+
+    return;
+}
+
+// duplicate files inside of directory
+void duplicateDir(int fs, superblock * sb, inode * in, char * dir_name, char * path){
+    // using directory inode
+    // traverse through directory and copy each file
+    // recurse when next entry in dir is also a directory
+
+    // create output directory
+
+    char new_path[4096];
+    new_path[0] = '\0';
+    strcat(new_path, path);
+
+    strcat(new_path, dir_name);
+    
+    mkdir(new_path, 0777);
+
+    strcat(new_path, "/");
+
+    uint bytes_read = 0;
+
+    // traverse through each entry 
+    //     if file: duplicate file
+    //     if directory: recurse
+
+    // direct pointers
+    for(int i = 0; i < 12; i++){
+        if(in->direct[i] == 0) continue; // skip empty inode
+
+        uint curr_addr = in->direct[i]*sb->block_sz;;
+        uint offset = 0;
+
+        // directory entry
+        while(bytes_read < in->file_sz && (offset < sb->block_sz)){ 
+            dir_entry * dir = getDirEntry(fs, sb, curr_addr+offset);
+                
+            offset += dir->size;
+            bytes_read += dir->size;
+
+            inode * in_2 = getInode(fs, sb, dir->inum);
+
+            // if file, duplicate to output
+            if(!in_2->isDir){
+                char new_file[4096];
+                new_file[0] = '\0';
+                strcat(new_file, new_path);
+                strcat(new_file, dir->name);
+
+                duplicateFile(fs, sb, in_2, new_file);
+            }
+            // if directory: recurse
+            else if(strcmp(dir->name, ".") && strcmp(dir->name, "..")){ // exclude parent and own directory
+                duplicateDir(fs, sb, in_2, dir->name, new_path);
+            }
+
+            freeDirEntry(dir);
+        }
+    }
+}
---
+++ src/navigate.c
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "duplicate.c"
+
+// for operation 2
+
+// file navigation:
+// 1. check root inode
+// 2. check root data dir
+// 3. check for filename
+// 4. repeat checking inode and data until file or directory is found
+
+// file names are separated by forward slashes
+
+// file path < 4096 characteers
+// file object name < 256 characters
+
+
+// clean filepath input string
+// by removing repeated slashes
+char * cleanInput(char * input){
+    int i = 0;
+    char * output = (char*) malloc((4096)*sizeof(char));
+
+    do{
+        output[i++] = '/';
+        
+        // ignore repeated slashes
+        while(*input != '\0' &&  *input == '/'){
+            input++;
+        }
+
+        char curr[256];
+        int j = 0;
+
+        while(*input != '\0' && *input != '/'){
+            output[i++] = *input;
+            input++;
+        }
+
+    }while(*(input++) != '\0');
+
+    return output;
+}
+
+
+// searches directory data block for file
+uint searchBlock(int fs, superblock * sb, inode * in, uint curr_addr, char * filename, uint * bytes_read, uint wantDir){
+    uint offset = 0;
+    
+    while(*bytes_read < in->file_sz && (offset < sb->block_sz)){ 
+        dir_entry * dir = getDirEntry(fs, sb, curr_addr+offset);
+            
+        offset += dir->size;
+        *bytes_read += dir->size;
+
+        if(!strcmp(filename, dir->name)){ // file is found
+            // get inode and check if directory
+
+            if(wantDir){
+                inode * dir_in = getInode(fs, sb, dir->inum);
+                if(dir_in->isDir){ // path is found
+                    uint result = dir->inum;
+
+                    freeDirEntry(dir);
+                    free(dir_in);
+
+                    return result;
+                }
+                free(dir_in);
+            }
+            else{
+                uint result = dir->inum;
+                freeDirEntry(dir);
+                return result;
+            }
+        }
+
+        freeDirEntry(dir);
+    }
+
+    return 0;
+}
+
+
+uint searchSingleIndirectDir(int fs, superblock * sb, inode * in, int block_num, char * filename, int * bytes_read, int wantDir){
+    uint result;
+
+    //iterate through each pointer
+    for(int i = 0; i < sb->block_sz; i+=4){
+        uint curr_offset = (block_num*sb->block_sz)+i; // offset of direct pointer
+        uint curr_addr = readInt(fs, curr_offset, 4) * 4096; // address of data block
+
+        //skip empty pointers
+        if(curr_addr == 0) continue;
+
+        result = searchBlock(fs, sb, in, curr_addr, filename, bytes_read, wantDir);
+
+        if(result != 0) return result;
+    }
+
+    return 0;
+}
+
+
+// searches directory and returns inode number of entry found
+// additionally parameter to indicate if looking for directory
+// returns 0 if not found (should be fine since block zero always contains the superblock)
+uint searchDir(int fs, superblock * sb, inode * in, char * filename, int wantDir){
+    // search in directory
+    //inode direct pointers
+
+    uint bytes_read = 0; // total number of bytes read 
+    uint curr_addr;
+    uint result;
+
+    // for each direct block
+    for(int i = 0; i < 12; i++){
+        if(in->direct[i] == 0) continue; // skip empty inode
+        
+        curr_addr = in->direct[i]*sb->block_sz;
+
+        result = searchBlock(fs, sb, in, curr_addr, filename, &bytes_read, wantDir);
+
+        if(result != 0) return result;
+    }
+
+    //inode single indirect
+    if(in->single_ind){
+        result = searchSingleIndirectDir(fs, sb, in, in->single_ind, filename, &bytes_read, wantDir);
+        if(result != 0) return result;
+    }
+
+    //double
+    if(in->double_ind){ 
+        // handler for double indirect block
+        // contains pointers to block containing singly indirect blocks
+
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get double block
+
+            uint d_block = (in->double_ind*sb->block_sz)+i; // address of direct pointer
+            uint d_block_num = readInt(fs, d_block, 4); // data block number
+
+            result = searchSingleIndirectDir(fs, sb, in, d_block_num, filename, &bytes_read, wantDir);
+            if(result != 0) return result;
+        }
+    }
+
+    //triple
+    if(in->triple_ind){
+        // handler for triple indirect block
+        // contains pointers to blocks containing doubly indirect blocks
+
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get triple block
+            uint t_block = (in->triple_ind*sb->block_sz)+i;
+            uint t_block_num = readInt(fs, t_block, 4); // data block number
+
+            for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+                uint d_block = (t_block_num*sb->block_sz)+i; // address of direct pointer
+                uint d_block_num = readInt(fs, d_block, 4); // data block number
+
+                result = searchSingleIndirectDir(fs, sb, in, d_block_num, filename, &bytes_read, wantDir);
+                if(result != 0) return result;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// takes in FILE pointer and path as inputs
+int navigate(int fs, char * path){ 
+    superblock * sb = parseSuperBlock(fs); // get superblock info
+    inode * in = getInode(fs, sb, 2); // get root inode
+    int result;
+
+    uchar data_buffer[sb->block_sz];
+
+    path = cleanInput(path);
+
+    // if root directory
+    if(!strcmp(path, "/")){
+        duplicateDir(fs, sb, getInode(fs, sb, 2), "output", "");
+        return 0;
+    }
+
+    for(int i = 1; i < strlen(path); i++){ // iterates through each filename in path
+        char filename[256];
+
+        // copies filename from path
+        int j = 0;
+        while(i < strlen(path) && path[i] != '/' ){
+            filename[j++] = path[i++];
+        }
+        filename[j] = '\0';
+
+        // check if filename is last in path, account for trailing '/'
+        if(i == strlen(path) || (i == strlen(path) -1 && path[i] == '/')){
+            result = searchDir(fs, sb, in, filename, 0);
+
+            if(result  == 0){
+                fprintf(stderr, "INVALID PATH\n");
+                return -1;
+            }
+            else{
+                inode * file_in = getInode(fs, sb, result);
+
+                //if directory
+                if(file_in->isDir){
+                    duplicateDir(fs, sb, file_in, filename, "output");
+                    return 0;
+                }
+                // regular file
+                else{
+                    duplicateFile(fs, sb, file_in, filename);
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+
+        result = searchDir(fs, sb, in, filename, 1);
+
+        if(result == 0) {
+            fprintf(stderr, "INVALID PATH\n");
+            return -1;
+        }
+        else{
+            free(in);
+            in = getInode(fs, sb, result);
+        }
+        
+    }
+}
