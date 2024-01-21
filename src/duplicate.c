d35491911436b2f14340c045717326ee3c091a24
dddelacruz <dddelacruz7@up.edu.ph>
2024-01-21 13:15:40 +0800

    fixed implementation of duplicateDir

--- src/duplicate.c
+++ src/duplicate.c
@@ -95,6 +95,54 @@ void duplicateFile(int fs, superblock * sb, inode * in, char * name){
     return;
 }
 
+void duplicateDir(int, superblock *, inode *, char *, char *);
+
+// helper for duplicate dir to copy directory entry blocks
+void copyDir(int fs, superblock * sb, inode * in, int block_num, int * bytes_read, char * new_path){
+    uint curr_addr = block_num*sb->block_sz;
+    uint offset = 0;
+
+        // directory entry
+    while(*bytes_read < in->file_sz && (offset < sb->block_sz)){ 
+        dir_entry * dir = getDirEntry(fs, sb, curr_addr+offset);
+                
+        offset += dir->size;
+        *bytes_read += dir->size;
+
+        inode * in_2 = getInode(fs, sb, dir->inum);
+
+        // if file, duplicate to output
+        if(!in_2->isDir){
+            char new_file[4096];
+            new_file[0] = '\0';
+            strcat(new_file, new_path);
+            strcat(new_file, dir->name);
+
+            duplicateFile(fs, sb, in_2, new_file);
+        }
+        // if directory: recurse
+        else if(strcmp(dir->name, ".") && strcmp(dir->name, "..")){ // exclude parent and own directory
+            duplicateDir(fs, sb, in_2, dir->name, new_path);
+        }
+
+        free(in_2);
+
+        freeDirEntry(dir);
+    }
+}
+
+void copySingleIndDir(int fs, superblock * sb, inode * in, int block_num, int * bytes_read, char * new_path){
+    for(int i = 0; i < sb->block_sz; i+=4){
+        uint curr_offset = (block_num*sb->block_sz)+i;
+        uint curr_block = readInt(fs, curr_offset, 4);
+
+        //skip empty pointers
+        if(curr_block == 0) continue;
+
+        copyDir(fs, sb, in, curr_block, bytes_read, new_path);
+    }
+}
+
 // duplicate files inside of directory
 void duplicateDir(int fs, superblock * sb, inode * in, char * dir_name, char * path){
     // using directory inode
@@ -123,33 +171,47 @@ void duplicateDir(int fs, superblock * sb, inode * in, char * dir_name, char * p
     for(int i = 0; i < 12; i++){
         if(in->direct[i] == 0) continue; // skip empty inode
 
-        uint curr_addr = in->direct[i]*sb->block_sz;;
-        uint offset = 0;
+        copyDir(fs, sb, in, in->direct[i], &bytes_read, new_path);   
+    }
 
-        // directory entry
-        while(bytes_read < in->file_sz && (offset < sb->block_sz)){ 
-            dir_entry * dir = getDirEntry(fs, sb, curr_addr+offset);
-                
-            offset += dir->size;
-            bytes_read += dir->size;
+    // indirect pointers
+    // single
+    if(in->single_ind){
+        copySingleIndDir(fs, sb, in, in->single_ind, &bytes_read, new_path);
+    }
 
-            inode * in_2 = getInode(fs, sb, dir->inum);
+    //double
+    if(in->double_ind){ 
+        // handler for double indirect block
+        // contains pointers to block containing singly indirect blocks
 
-            // if file, duplicate to output
-            if(!in_2->isDir){
-                char new_file[4096];
-                new_file[0] = '\0';
-                strcat(new_file, new_path);
-                strcat(new_file, dir->name);
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get double block
 
-                duplicateFile(fs, sb, in_2, new_file);
-            }
-            // if directory: recurse
-            else if(strcmp(dir->name, ".") && strcmp(dir->name, "..")){ // exclude parent and own directory
-                duplicateDir(fs, sb, in_2, dir->name, new_path);
-            }
+            uint d_block = (in->double_ind*sb->block_sz)+i; // address of direct pointer
+            uint d_block_num = readInt(fs, d_block, 4); // data block number
 
-            freeDirEntry(dir);
+           copySingleIndDir(fs, sb, in, d_block_num, &bytes_read, new_path);
         }
     }
+
+    //triple
+    if(in->triple_ind){
+        // handler for triple indirect block
+        // contains pointers to blocks containing doubly indirect blocks
+
+        for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+            // get triple block
+            uint t_block = (in->triple_ind*sb->block_sz)+i;
+            uint t_block_num = readInt(fs, t_block, 4); // data block number
+
+            for(int i = 0; i < sb->block_sz; i+= 4){ // pointer size is 4 bytes
+                uint d_block = (t_block_num*sb->block_sz)+i; // address of direct pointer
+                uint d_block_num = readInt(fs, d_block, 4); // data block number
+
+                copySingleIndDir(fs, sb, in, d_block_num, &bytes_read, new_path);
+            }
+        }
+    }
+
 }
